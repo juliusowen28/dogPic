c6c71500a3f6eceff28232e8fdef02fd25880115
Julius Owen Suherman <juliusowen28@gmail.com>
2022-08-09 10:56:52 -0700

    Free Dog Pictures

---
+++ dogPic/Model/Response.swift
@@ -0,0 +1,12 @@
+//
+//  Response.swift
+//  dogPic
+//
+//  Created by Julius Owen Suherman on 8/9/22.
+//
+
+import Foundation
+struct Response: Decodable{
+    var message:String
+    var status:String
+}
---
+++ dogPic/View/ContentView.swift
@@ -0,0 +1,40 @@
+//
+//  ContentView.swift
+//  dogPic
+//
+//  Created by Julius Owen Suherman on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @ObservedObject var dogPicVM = DogPicViewModel()
+    var body: some View {
+        VStack{
+            Text("Photo Album")
+                .font(.headline)
+                .padding()
+            Button("Add Photo", action:{self.dogPicVM.getDog()})
+                .padding()
+            List(self.dogPicVM.dogImageList,id:\.self){
+                dog in
+                AsyncImage(url: URL(string:dog)){
+                    image in
+                    image
+                        .resizable()
+                        .scaledToFill()
+                        .frame(maxWidth: .infinity)
+                        .clipped()
+                }placeholder: {
+                    ProgressView()
+                }
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
