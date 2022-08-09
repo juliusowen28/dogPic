11092a9b820d2a27647fb7fe4ad7385616688681
Julius Owen Suherman <juliusowen28@gmail.com>
2022-08-09 09:41:31 -0700

    Initial Commit

---
+++ dogPic/dogPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  dogPicApp.swift
+//  dogPic
+//
+//  Created by Julius Owen Suherman on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct dogPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
